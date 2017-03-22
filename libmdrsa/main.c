c6f6ddeb6e8f1af8f8d1d67d94c16b6052f35913
Mert Dümenci <mert@dumenci.me>
2017-03-22 15:04:20 -0700

    Fix comment line length

--- libmdrsa/main.c
+++ libmdrsa/main.c
@@ -10,23 +10,7 @@
 #include "bnhelpers.h"
 #include "prime.h"
 
-int main(int argc, const char * argv[]) {
-//    vU1024 base = bignum_from64(2);
-//    vU1024 power = bignum_from64(1023);
-//
-//    vU1024 result;
-//    vU1024 zero = bignum_from64(0);
-//    fastModuloPow(&base, &power, &zero, &result);
-//    bignum_printContents(&result);
-    
-//    vU1024 random = bignum_rand(200);
-//    bignum_printContents(&random);
-//
-//    vU1024 prime = bignum_from64(15487139);
-//    vU1024 composite = bignum_from64(15487140);
-//
-//    printf("Prime is %d, composite is %d\n", isPrime(&prime, 8), isPrime(&composite, 8));
-    
+int main(int argc, const char * argv[]) {    
     vU1024 prime = generatePrime(150);
     printf("Prime generated: %s\n", bignum_toString(&prime));
     
