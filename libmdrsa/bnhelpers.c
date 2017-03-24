fcdf951572149af557f72be947114861cff8dce4
Mert Dümenci <mert@dumenci.me>
2017-03-23 21:25:15 -0700

    Add signed bignum helpers

--- libmdrsa/bnhelpers.c
+++ libmdrsa/bnhelpers.c
@@ -13,6 +13,10 @@ bool bignum_equal(vU1024 *a, vU1024 *b) {
     return (memcmp(a, b, sizeof(vU1024)) == 0);
 }
 
+bool bignum_signedEqual(vS1024 *a, vS1024 *b) {
+    return (memcmp(a, b, sizeof(vS1024)) == 0);
+}
+
 bool bignum_isZero(vU1024 *bignum) {
     vU1024 zero = bignum_from64(0);
     return bignum_equal(bignum, &zero);
@@ -61,6 +65,30 @@ char *bignum_toString(vU1024 *bignum) {
     return string;
 }
 
+char *bignum_signedToString(vS1024 *bignum) {
+    bool isNegative = (bignum->s.MSW & 0x10000000) != 0;
+    
+    vU1024 positive;
+    
+    if (!isNegative) {
+        memcpy(&positive, bignum, sizeof(vS1024));
+        return bignum_toString(&positive);
+    }
+    
+    vU1024 unsignedBignum;
+    memcpy(&unsignedBignum, bignum, sizeof(vS1024));
+    
+    // I don't even know why `vU1024Neg` isn't a 1's complement negation,
+    // since it takes an unsigned number and outputs an unsigned number...
+    vU1024Neg(&unsignedBignum, &positive);
+    
+    char *positiveRepresentation = bignum_toString(&positive);
+    char *string = malloc(sizeof(char) + sizeof(positiveRepresentation));
+    strcpy(string + 1, positiveRepresentation);
+    string[0] = '-';
+    
+    return string;
+}
 
 void bignum_printContents(vU1024 *bignum) {
     char *string = bignum_toString(bignum);
@@ -75,10 +103,32 @@ vU1024 bignum_from64(uint64_t integer) {
     return bignum;
 }
 
+vS1024 bignum_signedFrom64(int64_t integer) {
+    bool isNegative = integer & 0x1000000000000000;
+    
+    vS1024 bignum;
+    memset(&bignum, 0, sizeof(vS1024));
+    memcpy(&bignum, &integer, sizeof(int64_t));
+    
+    // Sign extension
+    if (isNegative) {
+        memset((((int64_t *)&bignum) + 1), 1, sizeof(vU1024) - 8);
+    }
+    
+    return bignum;
+}
+
 vU1024 bignum_rand(int digits) {
     int bits = (int)ceil((digits - 1) * log2(10));
     vU1024 rand;
     memset(&rand, 0, sizeof(vU1024));
     arc4random_buf(&rand, bits / 8);
     return rand;
+}
+
+vS1024 bignum_signed(vU1024 *unsignedBignum) {
+    vS1024 signedBignum;
+    memcpy(&signedBignum, unsignedBignum, sizeof(vU1024));
+    
+    return signedBignum;
 }
--- libmdrsa/bnhelpers.h
+++ libmdrsa/bnhelpers.h
@@ -13,11 +13,15 @@
 #include <Accelerate/Accelerate.h>
 
 bool bignum_equal(vU1024 *a, vU1024 *b);
+bool bignum_signedEqual(vS1024 *a, vS1024 *b);
 bool bignum_isZero(vU1024 *bignum);
 bool bignum_isOdd(vU1024 *bignum);
 char *bignum_toString(vU1024 *bignum);
+char *bignum_signedToString(vS1024 *bignum);
 void bignum_printContents(vU1024 *bignum);
 vU1024 bignum_from64(uint64_t integer);
+vS1024 bignum_signedFrom64(int64_t integer);
 vU1024 bignum_rand(int digits);
+vS1024 bignum_signed(vU1024 *unsignedBignum);
 
 #endif /* bnhelpers_h */
